a497edc608de623229d6caaf883a8be950bad78c
Piotr Majkrzak <piotr@majkrzak.dev>
2022-11-27 20:45:16 +0100

    Update status

--- ptm215b.cpp
+++ ptm215b.cpp
@@ -140,20 +140,22 @@ bool PTM215B::parse_device(const esp32_ble_tracker::ESPBTDevice &device) {
 
     ESP_LOGI(TAG, "%s: %s", device.address_str().c_str(), data_telegram.f.switch_status.to_string().c_str());
 
+    state_ = data_telegram.f.switch_status;
+
     if (bar_sensor_) {
-      bar_sensor_->publish_state(data_telegram.f.switch_status.press);
+      bar_sensor_->publish_state(state_.press);
     }
     if (a0_sensor_) {
-      a0_sensor_->publish_state(data_telegram.f.switch_status.A0);
+      a0_sensor_->publish_state(state_.A0);
     }
     if (a1_sensor_) {
-      a1_sensor_->publish_state(data_telegram.f.switch_status.A1);
+      a1_sensor_->publish_state(state_.A1);
     }
     if (b0_sensor_) {
-      b0_sensor_->publish_state(data_telegram.f.switch_status.B0);
+      b0_sensor_->publish_state(state_.B0);
     }
     if (b1_sensor_) {
-      b1_sensor_->publish_state(data_telegram.f.switch_status.B1);
+      b1_sensor_->publish_state(state_.B1);
     }
 
     break;
