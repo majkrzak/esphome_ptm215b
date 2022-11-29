ad925ed3f35526d2345de89c780e35cb68cfe155
Piotr Majkrzak <piotr@majkrzak.dev>
2022-11-29 15:14:20 +0100

    Chenge security_key config schema

--- binary_sensor.py
+++ binary_sensor.py
@@ -11,12 +11,36 @@ PTM215B = ptm215b_ns.class_(
     "PTM215B", esp32_ble_tracker.ESPBTDeviceListener, cg.Component
 )
 
+CONF_SECURITY_KEY = "security_key"
+
+
+def security_key(value):
+    value = cv.string_strict(value)
+    parts = value.split(":")
+    if len(parts) != 16:
+        raise cv.Invalid("Security Key must consist of 16 : (colon) separated parts")
+    parts_int = []
+    if any(len(part) != 2 for part in parts):
+        raise cv.Invalid(
+            "Security Key must be format XX:XX:XX:XX:XX:XX:XX:XX:XX:XX:XX:XX:XX:XX:XX:XX"
+        )
+    for part in parts:
+        try:
+            parts_int.append(int(part, 16))
+        except ValueError:
+            # pylint: disable=raise-missing-from
+            raise cv.Invalid(
+                "Security Key parts must be hexadecimal values from 00 to FF"
+            )
+    return parts_int
+
+
 CONFIG_SCHEMA = (
     cv.Schema(
         {
             cv.GenerateID(): cv.declare_id(PTM215B),
             cv.Required(CONF_MAC_ADDRESS): cv.mac_address,
-            cv.Optional(CONF_KEY): cv.hex_int,
+            cv.Optional(CONF_SECURITY_KEY): security_key,
             cv.Optional("bar"): binary_sensor.binary_sensor_schema(),
             cv.Optional("a0"): binary_sensor.binary_sensor_schema(),
             cv.Optional("a1"): binary_sensor.binary_sensor_schema(),
@@ -42,21 +66,17 @@ async def to_code(config):
         "b1": var.set_b1_sensor,
     }
 
-    cg.add(var.set_address(
-        cg.MockObj(
-                    "{"
-                    + ",".join(map(str, config[CONF_MAC_ADDRESS].parts))
-                    + "}"
-                )
-        ))
+    cg.add(
+        var.set_address(
+            cg.MockObj("{" + ",".join(map(str, config[CONF_MAC_ADDRESS].parts)) + "}")
+        )
+    )
 
     if CONF_KEY in config:
         cg.add(
             var.set_key(
                 cg.MockObj(
-                    "{"
-                    + ",".join(map(str, config[CONF_KEY].to_bytes(16, "big")))
-                    + "}"
+                    "{" + ",".join(map(str, config[CONF_KEY].to_bytes(16, "big"))) + "}"
                 )
             )
         )
